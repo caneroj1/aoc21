b9a100685a133eeb8ed8d6dd42cd79bede4dc990
Joseph Canero <jmc41493@gmail.com>
2021-12-01 00:22:01 -0500

    finish day1  and cleanup

--- src/main/scala/aoc/challenges/Day1.scala
+++ src/main/scala/aoc/challenges/Day1.scala
@@ -2,10 +2,26 @@ package aoc.challenges
 
 class Day1 extends Challenge {
   def part1(input: String): String = {
-    return ""
+    val ints = input.linesIterator.map(_.toInt).toArray
+    calculateValue(ints, 1)
   }
 
   def part2(input: String): String = {
-    return ""
+    val ints = input.linesIterator.map(_.toInt).toArray
+    calculateValue(ints, 3)
+  }
+
+  private def calculateValue(measurements: Array[Int], sliceSize: Int): String = {
+    var prevSumOpt: Option[Int] = None
+    var count = 0
+    for (i <- Range(0, measurements.length - (sliceSize - 1))) {
+      val currentSum = measurements.slice(i, i + sliceSize).sum
+      prevSumOpt match {
+        case Some(prevSum) if currentSum > prevSum => count += 1
+        case _ =>
+      }
+      prevSumOpt = Some(currentSum)
+    }
+    count.toString
   }
 }
