ee15926412fba9460322ab4e65986fd97cd52209
Joseph Canero <jmc41493@gmail.com>
2021-12-10 18:10:06 -0500

    solve day 10

--- src/main/scala/aoc/challenges/Day10.scala
+++ src/main/scala/aoc/challenges/Day10.scala
@@ -9,36 +9,54 @@ class Day10 extends Challenge {
 
   val CLOSING_CHARS = Set.from(")]}>")
   val SCORE_TABLE = Map(')' -> 3, ']' -> 57, '}' -> 1197, '>' -> 25137)
+  val CLOSING_SCORE_TABLE = Map(')' -> 1L, ']' -> 2L, '}' -> 3L, '>' -> 4L)
   val OPENING_TABLE = Map(')' -> '(', ']' -> '[', '}' -> '{', '>' -> '<')
+  val CLOSING_TABLE = OPENING_TABLE.map(_.swap)
 
   override def part1(input: String): String = {
     val result = input.linesIterator
-      .flatMap(l => getFirstCorruptedChar(l, List.empty))
-      .map(c => SCORE_TABLE.getOrElse(c, 0))
+      .map(l => getFirstCorruptedCharOrStack(l, List.empty))
+      .filter(_.isLeft)
+      .map { case Left(c) => SCORE_TABLE.getOrElse(c, 0) }
       .sum
     result.toString
   }
 
-  override def part2(input: String): String = ???
+  override def part2(input: String): String = {
+    val sortedScores = input.linesIterator
+      .map(l => getFirstCorruptedCharOrStack(l, List.empty))
+      .filter(_.isRight)
+      .map { case Right(s) =>
+        s.flatMap(CLOSING_TABLE.get(_))
+          .map(CLOSING_SCORE_TABLE.getOrElse(_, 0L))
+          .fold(0L) { case (acc, score) =>
+            5L * acc + score
+          }
+      }
+      .toVector
+      .sorted
+
+    sortedScores(sortedScores.length / 2).toString
+  }
 
   @tailrec
-  private def getFirstCorruptedChar(
+  private def getFirstCorruptedCharOrStack(
       line: String,
       stack: List[Char]
-  ): Option[Char] = {
+  ): Either[Char, List[Char]] = {
     if (line.isEmpty) {
-      return None
+      return Right(stack)
     }
 
     val char = line.head
     if (CLOSING_CHARS.contains(char)) {
       stack.headOption match {
         case Some(lastCloser) if OPENING_TABLE(char) == lastCloser =>
-          getFirstCorruptedChar(line.tail, stack.tail)
-        case _ => Some(char)
+          getFirstCorruptedCharOrStack(line.tail, stack.tail)
+        case _ => Left(char)
       }
     } else {
-      getFirstCorruptedChar(line.tail, stack.prepended(char))
+      getFirstCorruptedCharOrStack(line.tail, stack.prepended(char))
     }
   }
 }
